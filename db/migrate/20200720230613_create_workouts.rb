e62a3b5ca00fc4a2f744d5f45b31e4a5f1a47cb9
jordon_terry <jt5493@hotmail.com>
2020-07-20 16:06:37 -0700

    added workout resources

---
+++ db/migrate/20200720230613_create_workouts.rb
@@ -0,0 +1,10 @@
+class CreateWorkouts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :workouts do |t|
+      t.date :workout_date
+      t.integer :points_total
+
+      t.timestamps
+    end
+  end
+end
